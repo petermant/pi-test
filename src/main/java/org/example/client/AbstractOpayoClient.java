5b4d09531d3c0de005236e1d248592cf3a7769f8
Pete Mantell <pete@duesday.com>
2021-07-05 17:38:00 +0100

    Split out reusable parts into an AbstractOpayoClient ready for transaction API

---
+++ src/main/java/org/example/client/AbstractOpayoClient.java
@@ -0,0 +1,36 @@
+package org.example.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.Map;
+
+public class AbstractOpayoClient {
+    final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Value("${opayo.integration.key}") private String integrationKey;
+    @Value("${opayo.integration.password}") private String integrationPassword;
+
+    private String integrationCode;
+
+    @PostConstruct
+    private void combineIntegrationCodes() {
+        byte [] combined = (integrationKey + ":" + integrationPassword).getBytes();
+        integrationCode = Base64.getEncoder().encodeToString(combined);
+        logger.debug("{}: Created base 64 combined key: {}", this.getClass().getSimpleName(), integrationCode);
+    }
+
+    HttpEntity<Map<String, String>> createRequest(final Map<String, String> body) {
+        final HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        httpHeaders.setBasicAuth(integrationCode);
+
+        return new HttpEntity<>(body, httpHeaders);
+    }
+}
--- src/main/java/org/example/client/MerchantSessionClient.java
+++ src/main/java/org/example/client/MerchantSessionClient.java
@@ -1,47 +1,30 @@
 package org.example.client;
 
 import org.example.client.dtos.SessionKey;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.PostConstruct;
-import java.util.Base64;
 import java.util.Collections;
 import java.util.Map;
 
 @Component
-public class MerchantSessionClient {
-
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+public class MerchantSessionClient extends AbstractOpayoClient {
 
     @Autowired private RestTemplate restTemplate;
 
     @Value("${opayo.uri.session-key}") private String sessionKeyURI;
 
     @Value("${opayo.integration.vendor-name}") private String vendorName;
-    @Value("${opayo.integration.key}") private String integrationKey;
-    @Value("${opayo.integration.password}") private String integrationPassword;
-
-    private String integrationCode;
-
-    @PostConstruct
-    private void combineIntegrationCodes() {
-        byte [] combined = (integrationKey + ":" + integrationPassword).getBytes();
-        integrationCode = Base64.getEncoder().encodeToString(combined);
-        logger.debug("Created base 64 combined key: {}", integrationCode);
-    }
 
     public SessionKey getSessionKey() {
-        final HttpEntity<Map<String, String>> httpEntity = createRequest();
+        final Map<String, String> body = Collections.singletonMap("vendorName", vendorName);
+        final HttpEntity<Map<String, String>> httpEntity = createRequest(body);
 
         final ResponseEntity<SessionKey> response = restTemplate.postForEntity(sessionKeyURI, httpEntity, SessionKey.class);
 
@@ -54,14 +37,5 @@ public class MerchantSessionClient {
             logger.error("{} error retrieving session key. Http response object: {}", response.getStatusCode(), response);
             throw new RuntimeException("Error retrieving session key");
         }
-    }
-
-    private HttpEntity<Map<String, String>> createRequest() {
-        final HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        httpHeaders.setBasicAuth(integrationCode);
-
-        final Map<String, String> body = Collections.singletonMap("vendorName", vendorName);
-        return new HttpEntity<>(body, httpHeaders);
     }
 }
